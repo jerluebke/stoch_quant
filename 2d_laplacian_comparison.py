1222bf7b60d5a2071417fff994caefc4c40f322d
Jeremiah Lübke <jer.luebke@web.de>
2019-04-25 20:06:30 +0200

    added numba stencil and compiled wrapper - large error for large grid size

--- 2d_laplacian_comparison.py
+++ 2d_laplacian_comparison.py
@@ -1,14 +1,27 @@
 # -*- coding: utf-8 -*-
 
-from multiprocessing.managers import Namespace
+from timeit import timeit
 import numpy as np
 from scipy import signal, ndimage
+from numba import njit, stencil
 
 MAX, MIN = -5, 5
-SIZES = [100, 1_000, 10_000, 100_00, 1_000_000]
-ITERATIONS = 1000
-HEAD = "grid size: %d\nname\t\t|\ttime per run (sec)\t|max error\n------"
-RES = "%s\t|\t%s\t\t|\t%s"
+SIZES = [
+    #  100,
+    1_000,
+    10_000,
+    100_00,
+    #  1_000_000
+]
+STENCIL = np.array([
+    [0., 1., 0.],
+    [1.,-4., 1.],
+    [0., 1., 0.]
+])
+ITERATIONS = 10
+START = "======\n\nComparing implementations to compute the 2D-Laplacian\n\n\t%s\n\t%s\n\n======\n\n"
+HEAD = "grid size: %d\n|name\t\t\t|time per run (sec)\t|max error\n|------"
+RES = "|%s\t|%e\t\t|%e"
 DELIM = "\n\n=======\n\n"
 
 
@@ -26,8 +39,6 @@ def my_lap2d(a):
     """custom implementation for a 2d laplacian of an array `a`
     for reference: https://stackoverflow.com/a/4699973"""
     r, c = a.shape
-    dx = np.ones((1, c-1))
-    dy = np.ones((r-1, 1))
     L = np.zeros_like(a)
 
     # x component
@@ -47,32 +58,56 @@ def my_lap2d(a):
     return L
 
 
+@stencil
+def _numba_lap_stencil(a):
+    return a[-1,0] + a[0,-1] - 4*a[0,0] + a[1,0] + a[0,1]
+
+@njit
+def numba_lap_stencil(a, o):
+    _numba_lap_stencil(a, out=o)
+    return o
+
+
 FUNCS = {
-    "my 2d laplacian"   :   my_lap2d,
-    "signal.convolve2d" :   lambda a: signal.convolve2d(a, STENCIL, mode='same'),
-    "ndimage.convolve"  :   lambda a: ndimage.convolve(a, STENCIL, mode='constant'),
-    "ndimage.laplace"   :   lambda a: ndimage.laplace(a, mode='constant')
+    #  "my 2d laplacian "  :   my_lap2d,
+    #  "signal.convolve2d" :
+    #      lambda a, o: signal.convolve2d(a, STENCIL, mode='same'),
+    "ndimage.convolve"  :
+        lambda a, o: ndimage.convolve(a, STENCIL, mode='constant', output=o),
+    #  "ndimage.laplace " :
+    #      lambda a, o: ndimage.laplace(a, mode='constant', output=o),
+    "numba stencil\t"   :
+        #  lambda a, o: _numba_lap_stencil(a, out=o)
+        numba_lap_stencil
 }
 
 
 def main():
+    print(START % (f.__doc__, ddf.__doc__))
+
     for n in SIZES:
         step = (MAX-MIN) / n
-        X, Y = np.meshgrid[MAX:MIN:step]
+        X, Y = np.mgrid[MIN:MAX:step,MIN:MAX:step]
         fxy = f(X, Y)
         lap_ana = ddf(X, Y, fxy)
+        lap_num = np.zeros_like(fxy)
 
         print(HEAD % n)
 
-        for name, func in FUNCS:
-            t = timeit('f(a)', number=ITERATIONS,
-                       globals=Namespace(a=fxy, f=func)) / ITERATIONS
-            lap_num = func(fxy) / step**2
+        for name, func in FUNCS.items():
+            t = timeit('f(a, o)', number=ITERATIONS,
+                       setup='f(a, o)',
+                       globals=dict(a=fxy, f=func, o=lap_num)) / ITERATIONS
+            lap_num = func(fxy, lap_num) / step**2
             err = np.max(np.abs(lap_num-lap_ana))
 
             print(RES % (name, t, err))
 
         print(DELIM)
+
+
+if __name__ == "__main__":
+    main()
 
 
 #  vim: set ff=unix tw=79 sw=4 ts=8 et ic ai :
